a3bb8daa452db51a9e90096d255356eab9e26fc1
Akmal-Keisin <akmalkeisin@gmail.com>
2022-03-06 20:28:43 +0700

    making cart api

--- app/Http/Controllers/CartApiController.php
+++ app/Http/Controllers/CartApiController.php
@@ -23,7 +23,7 @@ class CartApiController extends Controller
         try {
             $cart = Cart::all();
             $data = [
-                'info' => 'Cart All Data',
+                'info' => 'All cart data',
                 'status' => Response::HTTP_OK,
                 'data' => $cart
             ];
@@ -45,15 +45,41 @@ class CartApiController extends Controller
      */
     public function store(Request $request)
     {
+        $cart = Cart::where('user_id', '=', $request->user_id)->get();
+        if (!empty($cart)) {
+            if ($cart->where('product_id', '=', $request->product_id)->first()) {
+                $data = [
+                    'info' => 'Product already in cart',
+                    'status' => Response::HTTP_OK
+                ];
+                return response()->json($data, Response::HTTP_OK);
+            }
+        }
         $validatedData = Validator::make($request->all(), [
             'product_id' => 'required|integer',
             'user_id' => 'required|integer',
             'qty' => 'required|integer',
         ]);
+        if ($validatedData->fails()) {
+            $data = [
+                'info' => 'Error',
+                'status' => Response::HTTP_BAD_REQUEST,
+                'data' => $validatedData->errors()
+            ];
+            return response()->json($data, Response::HTTP_BAD_REQUEST);
+        }
 
+        $formData = $request->all();
         $product = Product::findOrFail($request->product_id);
-        $validatedData['cost'] = $request->qty * $product->price;
-        $product = Product::create($validatedData);
+        if ($product->qty < $request->qty) {
+            $data = [
+                'info' => 'Stock is not enough for this product',
+                'status' => Response::HTTP_BAD_REQUEST
+            ];
+            return response()->json($data, Response::HTTP_BAD_REQUEST);
+        }
+        $formData['cost'] = $request->qty * $product->price;
+        $cart = Cart::create($formData);
 
         $data = [
             'info' => 'Cart added successfully',
@@ -81,7 +107,7 @@ class CartApiController extends Controller
         }
         try {
             $cart = Cart::where('user_id', '=', $id)->get();
-            if (isEmpty($cart)) {
+            if (empty($cart)) {
                 $data = [
                     'info' => 'Cart Is Empty',
                     'status' => Response::HTTP_OK,
@@ -95,6 +121,7 @@ class CartApiController extends Controller
             'data' => $cart
         ];
         return response()->json($data, Response::HTTP_OK);
+
         } catch (QueryException $e) {
             return response()->json([
                 'message' => 'Failed ' . $e->errorInfo
@@ -115,11 +142,27 @@ class CartApiController extends Controller
         $validatedData = Validator::make($request->all(), [
             'qty' => 'required|integer'
         ]);
+        if ($validatedData->fails()) {
+            $data = [
+                'info' => 'Error',
+                'status' => Response::HTTP_BAD_REQUEST,
+                'data' => $validatedData->errors()
+            ];
+            return response()->json($data, Response::HTTP_BAD_REQUEST);
+        }
+
         $cart = Cart::findOrFail($id);
         $product = Product::findOrFail($cart->product_id);
-        $cart->qty = $validatedData['qty'];
-        $cart->cost = $validatedData['qty'] * $product->price;
-        $cart->update($cart);
+        if ($request->qty > $product->stock) {
+            $data = [
+                'info' => 'Product stock is not enough',
+                'status' => Response::HTTP_BAD_REQUEST
+            ];
+            return response()->json($data, Response::HTTP_BAD_REQUEST);
+        }
+        $cart->qty = (int)$request->qty;
+        $cart->cost = $request->qty * $product->price;
+        $cart->save();
 
         $data = [
             'info' => 'Cart updated successfully',
